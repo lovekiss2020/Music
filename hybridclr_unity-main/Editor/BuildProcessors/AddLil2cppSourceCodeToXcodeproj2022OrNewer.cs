7b5e1c07eb46daa9d0a3f3eee765a47509cbb349
liufei <2634540032@qq.com>
2024-05-31 23:24:18 +0800

    unity 版本切换以及添加热更模块

---
+++ hybridclr_unity-main/Editor/BuildProcessors/AddLil2cppSourceCodeToXcodeproj2022OrNewer.cs
@@ -0,0 +1,61 @@
+using HybridCLR.Editor.Installer;
+using HybridCLR.Editor.Settings;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+#if UNITY_2022_2_OR_NEWER && (UNITY_IOS || UNITY_TVOS || UNITY_VISIONOS)
+
+namespace HybridCLR.Editor.BuildProcessors
+{
+    public static class AddLil2cppSourceCodeToXcodeproj2022OrNewer
+    {
+
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            if (!HybridCLRSettings.Instance.enable)
+                return;
+            string pbxprojFile = BuildProcessorUtil.GetXcodeProjectFile(pathToBuiltProject);
+            RemoveExternalLibil2cppOption(pbxprojFile);
+            CopyLibil2cppToXcodeProj(pathToBuiltProject);
+        }
+
+        private static void RemoveExternalLibil2cppOption(string pbxprojFile)
+        {
+            string pbxprojContent = File.ReadAllText(pbxprojFile, Encoding.UTF8);
+            string removeBuildOption = @"--external-lib-il2-cpp=\""$PROJECT_DIR/Libraries/libil2cpp.a\""";
+            if (pbxprojContent.Contains(removeBuildOption))
+            {
+                pbxprojContent = pbxprojContent.Replace(removeBuildOption, "");
+                Debug.Log($"[AddLil2cppSourceCodeToXcodeproj] remove il2cpp build option '{removeBuildOption}' from file '{pbxprojFile}'");
+            }
+            else
+            {
+                Debug.LogWarning($"[AddLil2cppSourceCodeToXcodeproj] project.pbxproj remove building option:'{removeBuildOption}' fail. This may occur when 'Append' to existing xcode project in building");
+            }
+
+            int strShellScriptIndex1 = pbxprojContent.IndexOf("/* ShellScript */,");
+            int strShellScriptIndex2 = pbxprojContent.IndexOf("/* ShellScript */,", strShellScriptIndex1 + 10);
+            if (strShellScriptIndex2 >= 0)
+            {
+                pbxprojContent = pbxprojContent.Remove(strShellScriptIndex1, strShellScriptIndex2 - strShellScriptIndex1);
+                Debug.LogWarning($"[AddLil2cppSourceCodeToXcodeproj] remove duplicated '/* ShellScript */' from file '{pbxprojFile}'");
+            }
+
+            File.WriteAllText(pbxprojFile, pbxprojContent, Encoding.UTF8);
+        }
+
+        private static void CopyLibil2cppToXcodeProj(string pathToBuiltProject)
+        {
+            string srcLibil2cppDir = $"{SettingsUtil.LocalIl2CppDir}/libil2cpp";
+            string destLibil2cppDir = $"{pathToBuiltProject}/Il2CppOutputProject/IL2CPP/libil2cpp";
+            BashUtil.RemoveDir(destLibil2cppDir);
+            BashUtil.CopyDir(srcLibil2cppDir, destLibil2cppDir, true);
+        }
+    }
+}
+#endif
---
+++ hybridclr_unity-main/Editor/ReversePInvokeWrap/Analyzer.cs
@@ -0,0 +1,161 @@
+﻿using dnlib.DotNet;
+using HybridCLR.Editor.ABI;
+using HybridCLR.Editor.Meta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CallingConvention = System.Runtime.InteropServices.CallingConvention;
+using UnityEngine;
+
+namespace HybridCLR.Editor.ReversePInvokeWrap
+{
+    public class RawReversePInvokeMethodInfo
+    {
+        public MethodDef Method { get; set; }
+
+        public CustomAttribute GenerationAttribute { get; set; }
+    }
+
+    public class ABIReversePInvokeMethodInfo
+    {
+        public MethodDesc Method { get; set; }
+
+        public CallingConvention Callvention { get; set; }
+
+        public int Count { get; set; }
+
+        public string Signature { get; set; }
+    }
+
+    public class Analyzer
+    {
+
+        private readonly List<ModuleDefMD> _rootModules = new List<ModuleDefMD>();
+
+        private readonly List<RawReversePInvokeMethodInfo> _reversePInvokeMethods = new List<RawReversePInvokeMethodInfo>();
+
+        public Analyzer(AssemblyCache cache, List<string> assemblyNames)
+        {
+            foreach (var assemblyName in assemblyNames)
+            {
+                _rootModules.Add(cache.LoadModule(assemblyName));
+            }
+        }
+
+        private void CollectReversePInvokeMethods()
+        {
+            foreach (var mod in _rootModules)
+            {
+                Debug.Log($"ass:{mod.FullName} methodcount:{mod.Metadata.TablesStream.MethodTable.Rows}");
+                for (uint rid = 1, n = mod.Metadata.TablesStream.MethodTable.Rows; rid <= n; rid++)
+                {
+                    var method = mod.ResolveMethod(rid);
+                    //Debug.Log($"method:{method}");
+                    if (!method.IsStatic || !method.HasCustomAttributes)
+                    {
+                        continue;
+                    }
+                    CustomAttribute wa = method.CustomAttributes.FirstOrDefault(ca => ca.AttributeType.Name == "MonoPInvokeCallbackAttribute");
+                    if (wa == null)
+                    {
+                        continue;
+                    }
+                    //foreach (var ca in method.CustomAttributes)
+                    //{
+                    //    Debug.Log($"{ca.AttributeType.FullName} {ca.TypeFullName}");
+                    //}
+                    _reversePInvokeMethods.Add(new RawReversePInvokeMethodInfo()
+                    {
+                        Method = method,
+                        GenerationAttribute = method.CustomAttributes.FirstOrDefault(ca => ca.AttributeType.FullName == "HybridCLR.ReversePInvokeWrapperGenerationAttribute"),
+                    });
+                }
+            }
+        }
+
+        private static string MakeSignature(MethodDesc desc, CallingConvention CallingConventionention)
+        {
+            string convStr = ((char)('A' + (int)CallingConventionention - 1)).ToString();
+            return $"{convStr}{desc.Sig}";
+        }
+
+        private static CallingConvention GetCallingConvention(MethodDef method)
+        {
+            var monoPInvokeCallbackAttr = method.CustomAttributes.FirstOrDefault(ca => ca.AttributeType.Name == "MonoPInvokeCallbackAttribute");
+            if (monoPInvokeCallbackAttr == null)
+            {
+                return CallingConvention.Winapi;
+            }
+            object delegateTypeSig = monoPInvokeCallbackAttr.ConstructorArguments[0].Value;
+
+            TypeDef delegateTypeDef;
+            if (delegateTypeSig is ClassSig classSig)
+            {
+                delegateTypeDef = classSig.TypeDefOrRef.ResolveTypeDefThrow();
+            }
+            else if (delegateTypeSig is GenericInstSig genericInstSig)
+            {
+                delegateTypeDef = genericInstSig.GenericType.TypeDefOrRef.ResolveTypeDefThrow();
+            }
+            else
+            {
+                delegateTypeDef = null;
+            }
+
+            if (delegateTypeDef == null)
+            {
+                throw new NotSupportedException($"Unsupported delegate type {delegateTypeSig.GetType()}");
+            }
+            var attr = delegateTypeDef.CustomAttributes.FirstOrDefault(ca => ca.AttributeType.FullName == "System.Runtime.InteropServices.UnmanagedFunctionPointerAttribute");
+            if (attr == null)
+            {
+                return CallingConvention.Winapi;
+            }
+            var conv = attr.ConstructorArguments[0].Value;
+            return (CallingConvention)conv;
+        }
+
+        public List<ABIReversePInvokeMethodInfo> BuildABIMethods()
+        {
+            var methodsBySig = new Dictionary<string, ABIReversePInvokeMethodInfo>();
+            var typeCreator = new TypeCreator();
+            foreach(var method in _reversePInvokeMethods)
+            {
+                MethodDesc desc = new MethodDesc
+                {
+                    MethodDef = method.Method,
+                    ReturnInfo = new ReturnInfo { Type = typeCreator.CreateTypeInfo(method.Method.ReturnType)},
+                    ParamInfos = method.Method.Parameters.Select(p => new ParamInfo { Type = typeCreator.CreateTypeInfo(p.Type)}).ToList(),
+                };
+                desc.Init();
+
+                CallingConvention callingConv = GetCallingConvention(method.Method);
+                string signature = MakeSignature(desc, callingConv);
+
+                if (!methodsBySig.TryGetValue(signature, out var arm))
+                {
+                    arm = new ABIReversePInvokeMethodInfo()
+                    {
+                        Method = desc,
+                        Signature = signature,
+                        Count = 0,
+                        Callvention = callingConv,
+                    };
+                    methodsBySig.Add(signature, arm);
+                }
+                int preserveCount = method.GenerationAttribute != null ? (int)method.GenerationAttribute.ConstructorArguments[0].Value : 1;
+                arm.Count += preserveCount;
+            }
+            var methods = methodsBySig.Values.ToList();
+            methods.Sort((a, b) => string.CompareOrdinal(a.Signature, b.Signature));
+            return methods;
+        }
+
+        public void Run()
+        {
+            CollectReversePInvokeMethods();
+        }
+    }
+}
